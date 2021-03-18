b230e9fadcc4d18a582f0fa6cdfdad9f9839f190
Jay Cazaubon <jotwco@gmail.com>
2021-03-17 22:51:51 -0400

    local database setup

--- dbconnect.js
+++ dbconnect.js
@@ -1,27 +1,16 @@
-let mysql = require('mysql');
+const mysql = require("mysql");
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'themiddlmanisthenextbigthing',
-    database: 'middlman'
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "themiddlmanisthenextbigthing",
+  database: "middlman",
 });
 
 connection.connect(function(err) {
-  if (err) {
-    return console.error('error: ' + err.message);
-  }
-
-  console.log('Connected to the MySQL server.');
-});
-
-// To close a database connection gracefully
-connection.end(function(err) {
-  if (err) {
-    return console.log('error:' + err.message);
-  }
-  console.log('Close the database connection.');
-});
-
-// To force the connection close immediately
-connection.destroy()
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
