846345ff040e04bd8cc3589f8e0bd976ddc15ea6
Jay Cazaubon <jotwco@gmail.com>
2021-08-04 22:10:55 -0400

    setup of radio buttons

--- middlman/pages/index.js
+++ middlman/pages/index.js
@@ -1,9 +1,34 @@
-import { Heading, Page } from "@shopify/polaris";
+import React, {useCallback, useState} from 'react';
+import {Stack, RadioButton} from '@shopify/polaris';
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+function Index() {
+  const [value, setValue] = useState('disabled');
+
+  const handleChange = useCallback(
+    (_checked, newValue) => setValue(newValue),
+    [],
+  );
+
+  return (
+    <Stack vertical>
+      <RadioButton
+        label="Middlman"
+        helpText="Get your package delivered to you within hours"
+        checked={value === 'disabled'}
+        id="disabled"
+        name="accounts"
+        onChange={handleChange}
+      />
+      <RadioButton
+        label="Shipping"
+        helpText="Wait longer to get your packages instead"
+        id="optional"
+        name="accounts"
+        checked={value === 'optional'}
+        onChange={handleChange}
+      />
+    </Stack>
+  );
+}
 
 export default Index;
